606d8d7b5839b50f5757cb7ff0898117a6439f8f
tony <marais.anthony.pro@gmail.com>
2020-10-14 17:22:12 +0200

    update databases

--- dataviz netflix/netflixDB.sql
+++ dataviz netflix/netflixDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `netflix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `netflix`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
 -- Host: localhost    Database: netflix
@@ -168,4 +170,4 @@ CREATE TABLE `DIRECTOR_CATALOGUE` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-14 16:55:18
+-- Dump completed on 2020-10-14 17:21:01
--- dataviz netflix/netflixDB_WITH_FULL_DATA.sql
+++ dataviz netflix/netflixDB_WITH_FULL_DATA.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `netflix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `netflix`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
 -- Host: localhost    Database: netflix
@@ -259,4 +261,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-14 16:54:25
+-- Dump completed on 2020-10-14 17:20:29
