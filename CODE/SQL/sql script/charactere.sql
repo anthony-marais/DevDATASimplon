013cb313b235ba58846a003dc6036fcfa1d21af1
Anthony <marais.anthony.pro@gmail.com>
2021-02-16 23:27:58 +0100

    UPDATE DU REPOSITORY DEV DATA

--- CODE/SQL/sql script/charactere.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE characteres (IN titre VARCHAR(128), OUT title_char VARCHAR(128))
-BEGIN
-	SELECT title 
-    FROM film
-    WHERE titre LIKE title_char;
-END
--- dossier .py/Demo_netflix/app/config.py
+++
@@ -1,6 +0,0 @@
-# coding: utf8
-import sys
-
-# Modifier la config pour 
-# Capturer tous les logs
-configuration = {'engine_netflix':'mysql+pymysql://root:Leonne00@localhost/netflix'}
